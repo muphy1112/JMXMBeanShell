527ed7b83fc29e146afc434019397f79b9ebdff0
ruphy <ymh19941112@outlook.com>
2020-11-14 01:18:07 +0800

    添加JMX利用代码

---
+++ src/me/muphy/mbean/MLetMBeanServer.java
@@ -0,0 +1,20 @@
+package me.muphy.mbean;
+
+import javax.management.MBeanServer;
+import javax.management.remote.JMXConnectorServer;
+import javax.management.remote.JMXConnectorServerFactory;
+import javax.management.remote.JMXServiceURL;
+import java.lang.management.ManagementFactory;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class MLetMBeanServer {
+    public static void main(String[] args) throws Exception {
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        Registry registry = LocateRegistry.createRegistry(1099);
+        JMXServiceURL jmxServiceURL = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:1099/jmxrmi");
+        JMXConnectorServer jmxConnectorServer = JMXConnectorServerFactory.newJMXConnectorServer(jmxServiceURL, null, mBeanServer);
+        jmxConnectorServer.start();
+        System.out.println("JMXConnectorServer is running");
+    }
+}
